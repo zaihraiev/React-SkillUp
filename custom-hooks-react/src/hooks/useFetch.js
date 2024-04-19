73e38b7aa29f034deb5fa498122d1cadf54aad1e
nixzaihraiev <dmytro.zaihraiev@nixs.com>
2024-04-19 11:13:59 +0200

    Worked with custom react hooks

---
+++ custom-hooks-react/src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+﻿import { useEffect, useState } from "react";
+import { fetchUserPlaces } from "../http.js";
+
+export function useFetch(fetchDataFunc, initialValue) {
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchDataFunc();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchDataFunc]);
+
+  return { fetchedData, setFetchedData, isFetching, error };
+}
